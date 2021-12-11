b8240c8a69686709619a23d7142ff489f717edcc
AlejandroV01 <alexvera0109@gmail.com>
2021-12-10 21:41:42 -0500

    KOD

--- src/HomeAgain.js
+++ src/HomeAgain.js
@@ -16,43 +16,43 @@ import logoish from './assets/images/logoish.png'
 //import graylinevertical from './assets/images/graylinevertical.png'
 
 function HomeAgain() {
-    const [timerDays, setTimerDays] = useState();
-    const [timerHours, setTimerHours] = useState();
-    const [timerMinutes, setTimerMinutes] = useState();
-    const [timerSeconds, setTimerSeconds] = useState();
+    // const [timerDays, setTimerDays] = useState();
+    // const [timerHours, setTimerHours] = useState();
+    // const [timerMinutes, setTimerMinutes] = useState();
+    // const [timerSeconds, setTimerSeconds] = useState();
 
-    let interval;
+    // let interval;
 
-    const startTimer = () => {
-        const countDownDate = new Date("December 28,2021 ").getTime();
+    // const startTimer = () => {
+    //     const countDownDate = new Date("December 28,2021 ").getTime();
 
-        interval = setInterval(() => {
-            const now = new Date().getTime();
+    //     interval = setInterval(() => {
+    //         const now = new Date().getTime();
 
-            const distance = countDownDate - now;
+    //         const distance = countDownDate - now;
 
-            const days = Math.floor(distance / (24 * 60 * 60 * 1000));
-            const hours = Math.floor((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
-            const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+    //         const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+    //         const hours = Math.floor((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));
+    //         const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+    //         const seconds = Math.floor((distance % (60 * 1000)) / 1000);
 
-            if (distance < 0) {
-                //stop Timer
+    //         if (distance < 0) {
+    //             //stop Timer
 
-                clearInterval(interval.current);
-            } else {
-                // Update timer
-                setTimerDays(days);
-                setTimerHours(hours);
-                setTimerMinutes(minutes);
-                setTimerSeconds(seconds);
-            }
-        })
-    };
+    //             clearInterval(interval.current);
+    //         } else {
+    //             // Update timer
+    //             setTimerDays(days);
+    //             setTimerHours(hours);
+    //             setTimerMinutes(minutes);
+    //             setTimerSeconds(seconds);
+    //         }
+    //     })
+    // };
 
-    useEffect(() => {
-        startTimer();
-    });
+    // useEffect(() => {
+    //     startTimer();
+    // });
     return (
 
         <div class='whole'>
