6c22d6542ae5d7e65659161dca440a853c278adc
AlejandroV01 <alexvera0109@gmail.com>
2021-12-10 21:44:00 -0500

    KOD2

--- src/HomeAgain.js
+++ src/HomeAgain.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './HomeAgain.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 //import bullgif from './assets/images/bullgif.gif'
